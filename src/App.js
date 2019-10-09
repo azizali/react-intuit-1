545d7bfb29678b53c936caf8ed282a897cf5dee8
Aziz Ali <azizali@Azizs-MacBook-Pro.local>
2019-10-09 14:45:21 -0700

    Styling components

--- src/App.js
+++ src/App.js
@@ -1,31 +1,13 @@
 import React from 'react';
 import Wrapper from './Wrapper';
-import Content from './Content'
-import DemoJSX from './DemoJSX'
+import Content from './Content';
 
-// Capitalized
-// Return a user Interface
-// One parent
 export default function App (){
   return (
     <div>
-      <DemoJSX />
-      {/* WRAPPER A: named as prop */}
-      <Wrapper name="Wrapper A" children={()=>{ console.log('hi')}}>
-        {()=>{ console.log('hi')}}
-      </Wrapper>
-      
-      {/* WRAPPER B: Most common usecase */}
       <Wrapper name="Wrapper B">
-        Some words over here in B
         <Content />
-        Some additional stuff
       </Wrapper>
-
-      {/* WRAPPER C */}
-      {/* <Wrapper name="Wrapper C">
-        {()=>{console.log('hello C')}}
-      </Wrapper> */}
     </div>
   )
 }
--- src/Footer.js
+++ src/Footer.js
@@ -1,9 +1,20 @@
+import './footer.css';
 import React from 'react';
 
 export default function Footer (){
   return (
-    <div>
+    <div
+      className="full-width"
+      style={{
+        backgroundColor: 'blue',
+        borderRadius: '10px',
+      }}
+      // style="background-color: blue"
+    >
       I AM FOOTER
+      <div className="col">
+        Something
+      </div>
     </div>
   )
 }

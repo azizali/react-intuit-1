3badc67502d6137b582cbe6c9b750978fa0c3dc9
Aziz Ali <azizali@Azizs-MacBook-Pro.local>
2019-10-09 12:39:37 -0500

    From Scratch

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
+// React is just a library for creating components
+
+// react-dom
+// DOM Document Object Model = Browser
+
+// react-native
+// Mobile Apps.
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+// Capitalized
+// Return a user Interface
+// One parent
+function App (){
+  return (
+    <div>
+      <div>First Div</div>
+      <div>Second Div</div>
+    </div>
+  )
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();

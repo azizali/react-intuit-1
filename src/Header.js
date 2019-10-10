0e3268fa7679a9887851a304c824af67325ffc4f
Aziz Ali <azizali@Azizs-MacBook-Pro.local>
2019-10-09 23:47:55 -0500

    Expression and conditional renders

--- src/Header.js
+++ src/Header.js
@@ -4,12 +4,22 @@ export default function Header (props){
   const value = props.getInfo();
   
   return (
-    <div>
+    <div>   
       Hello {props.name} <br />
-      Age: {props.age} <br />
-      Employees: {props.employees} <br />
+      Age: {props.age * 3} <br />
+      Valid: {(function(){ return 'abc'})()} <br />
+      
+      {/* // Invalid since its not an expression */}
+      {/* Valid?: {function(){ return 'abc'}} <br /> */}
+      
+      {(props.age > 21)? 'You are allowed access': 'You are underage'}<br />
+      
+      {!!props.employees && <>Employees: {props.employees}<br /></>}
+      
       Office Count: {props.options.numberOfOffices}<br />
+      
       Employee Count: {props.options.numberOfEmployees}<br />
+      
       getInfo: {value} <br />
       <hr />
     </div>
